50806ae7bb1777a1f6ab9a11a3562cfa1dd31e0c
병곤 강 <kangbk4182@naver.com>
2021-07-08 20:22:34 +0900

    5. Entity Framework Core 참조 및 데이터 베이스 컨테스트 클래스 생성

---
+++ ArticleApp.Models/ArticleAppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ArticleApp.Models
+{
+    /// <summary>
+    /// DbContext Class
+    /// </summary>
+    public class ArticleAppDbContext : DbContext
+
+    {
+        //EfCore.SqlServer
+        //EfCore.InMemory
+        //Configuration.ConfigurationManager
+        public ArticleAppDbContext()
+        {
+            //Empty
+        }
+
+        public ArticleAppDbContext(DbContextOptions<ArticleAppDbContext> options)
+            :base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Article>().Property(m => m.Created).HasDefaultValueSql("GetDate()");
+        }
+
+        //[!] ArticleApp 관련 모든 테이블 참조 
+        public DbSet<Article> Articles { get; set; }
+
+    }
+}
