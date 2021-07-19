d8ef92566f32308fd099c7d812f35baab66610fa
병곤 강 <kangbk4182@naver.com>
2021-07-20 00:15:52 +0900

    013. 게시판 수정 페이지 작성

--- ArticleApp.SqlServer/dbo/Tables/Articles.sql
+++ ArticleApp.SqlServer/dbo/Tables/Articles.sql
@@ -4,6 +4,7 @@ CREATE TABLE [dbo].[Articles]
 	[Id] Int Not Null Primary Key Identity(1, 1), --일련번호 
 	[Title] NVarChar(255) Not Null,				  --제목 
 
+	[Content] NVarChar(Max) Null, --내용 
 	[CreatedBy] NVarChar(255) Null, --등록자(Creator)
 	[Created] DateTime Default(GetDate()), --생성일
 	[ModifiedBy] NVarChar(255) Null, --수정자(LastModifiedBy)
